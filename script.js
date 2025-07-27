527b4e2b685e7e69036c578192c2364334d5166f
Younes Darabi <younes.darabi@outlook.com>
2025-07-27 17:48:33 +0200

    add Calculator

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function submit(event){﻿
-    event.preventDefault();
-    const data = new FormData(event.target);
-
-    fetch("https://formspree.io/f/xyzpedkz", {
-        method: "POST",
-        body: new FormData(event.target),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(() => {
-        window.location.href = "/kontakt-send-mail.html";
-    }).catch((error) => {
-        console.log(error);
-    });
+function Calculator() {
+    let sum = document.getElementById("text-box").value;
+    document.getElementById('td1').innerHTML = sum * 60 + "g Mascarpone";
+    if (sum>1){
+        document.getElementById('td2').innerHTML = sum + " frische Eier";
+    }else{
+        document.getElementById('td2').innerHTML = sum + " frische Ei";
+    }
+    document.getElementById('td3').innerHTML = sum * 25 + "g Zucker";
+    document.getElementById('td4').innerHTML = sum * 50 + "g Löffelbiskuits (Savoiardi)";
+    document.getElementById('td5').innerHTML = sum + 50 + " ml starker Espresso (abgekühlt)";
+    document.getElementById('td6').innerHTML = sum  + " EL Amaretto oder Marsala (optional)";
+    document.getElementById('td7').innerHTML = sum * 1/2 + " Päckchen Vanillezucker";
 }
